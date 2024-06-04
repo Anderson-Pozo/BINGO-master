f46e52efb8104c9c316460505dece00d01e1e5c0
Paul Alvarez Corral <paul_alvarez11@outlook.com>
2024-06-03 20:39:05 -0500

    spaces

--- src/views/pages/login/login/PasswordRecover.js
+++ src/views/pages/login/login/PasswordRecover.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 // material-ui
 import { useTheme } from '@mui/material/styles';
 import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
-
 // project imports
 import AuthWrapper1 from '../AuthWrapper';
 import AuthCardWrapper from '../AuthCardWrapper';
