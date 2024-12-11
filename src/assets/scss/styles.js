d9e554d4b455648671fe373bf8edf1e0854f9ffd
Anderson Pozo <anderson.pozo@upec.edu.ec>
2024-12-11 12:30:17 -0500

    refactor: enhance modal styles with scrollbar customization and size constraints

--- src/assets/scss/styles.js
+++ src/assets/scss/styles.js
@@ -10,5 +10,23 @@ export const modalStyle = {
   border: 'none',
   borderRadius: 6,
   boxShadow: 10,
-  p: 4
+  p: 4,
+  maxWidth: '500px',
+  maxHeight: '90vh',
+  overflow: 'auto',
+  '&::-webkit-scrollbar': {
+    width: '8px'
+  },
+  // '&::-webkit-scrollbar-thumb': {
+  //   backgroundColor: '#888',
+  //   borderRadius: '4px'
+  // },
+  // '&::-webkit-scrollbar-thumb:hover': {
+  //   backgroundColor: '#555'
+  // },
+  '&::-webkit-scrollbar-track': {
+    borderRadius: '4px',
+    border: 'none',
+    background: 'transparent'
+  }
 };
